4baad0d08ff9d5ae779b569f4834b7c5004960f0
shineceo97 <shineceo97@naver.com>
2019-11-07 18:33:14 +0900

    ViewPager & bottom 바 구현 완료 (이미지 하고 세부 디자인 변경해야 함.)

--- app/src/main/java/com/example/jarvis/MainActivity.java
+++ app/src/main/java/com/example/jarvis/MainActivity.java
@@ -1,14 +1,76 @@
 package com.example.jarvis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.skydoves.colorpickerview.ColorPickerView;
+import com.skydoves.colorpickerview.sliders.BrightnessSlideBar;
+import com.yinglan.alphatabs.AlphaTabView;
+import com.yinglan.alphatabs.AlphaTabsIndicator;
 
+
+public class MainActivity extends AppCompatActivity
+{
+    ViewPager vp;
+    private AlphaTabsIndicator alphaTabsIndicator;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vp = (ViewPager)findViewById(R.id.mViewPager);
+
+        vp.setAdapter(new pagerAdapter(getSupportFragmentManager()));
+        vp.setCurrentItem(0);
+
+        alphaTabsIndicator = (AlphaTabsIndicator) findViewById(R.id.alphaIndicator);
+        alphaTabsIndicator.setViewPager(vp);
+
+    }
+
+    View.OnClickListener movePageListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            int tag = (int) v.getTag();
+            vp.setCurrentItem(tag);
+        }
+    };
+
+    private class pagerAdapter extends FragmentStatePagerAdapter
+    {
+        public pagerAdapter(FragmentManager fm)
+        {
+            super(fm);
+        }
+        @Override
+        public Fragment getItem(int position)
+        {
+            switch(position)
+            {
+                case 0:
+                    return new FragColor();
+                case 1:
+                    return new FragDust();
+                case 2:
+                    return new FragMode();
+                default:
+                    return null;
+            }
+        }
+        @Override
+        public int getCount()
+        {
+            return 3;
+        }
     }
 }
